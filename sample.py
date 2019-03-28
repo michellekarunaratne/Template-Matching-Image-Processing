3e95a7ea319c9d3d04e3fbbad8e53fe1f2dce09c
Michelle <msumalini@gmail.com>
2019-03-28 13:32:48 +0530

    optimizing the code

--- sample.py
+++ sample.py
@@ -4,15 +4,16 @@ import numpy as np
 
 
 #reading images
-imageGrey=cv2.imread('room.png',0)
-templateGrey=cv2.imread('mask.png',0)
+imageGrey=cv2.imread('page.png',0)
+templateGrey=cv2.imread('letter.png',0)
 
 #template matching
 result=cv2.matchTemplate(imageGrey,templateGrey,cv2.TM_SQDIFF_NORMED)
 
 #finding the occurences of the template
 threshold=0.0135
-templateHeight,templateWidth= templateGrey.shape[::1]
+templateHeight,templateWidth= templateGrey.shape
+
 #checking the normalized squared difference value is smaller than threshold value so thats its closer to the template or equal to the template
 location=np.where(result <= threshold)
 
@@ -20,6 +21,7 @@ location=np.where(result <= threshold)
 for point in zip(*location[::-1]):
     cv2.rectangle(imageGrey, point,(point[0]+templateWidth,point[1]+templateHeight),0,2)
 
+#Displaying the image
 cv2.imshow("Detected Template",imageGrey)
 cv2.imshow("Template",templateGrey)
 cv2.waitKey(0)
