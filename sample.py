f332677934c25d50f46f4ee3626e10bfaf56a0fb
Michelle <msumalini@gmail.com>
2019-03-30 20:45:38 +0530

    added a mask to the final image output

--- sample.py
+++ sample.py
@@ -1,5 +1,4 @@
 import  cv2
-from matplotlib import pyplot as plt
 import numpy as np
 
 
@@ -12,16 +11,24 @@ result=cv2.matchTemplate(imageGrey,templateGrey,cv2.TM_SQDIFF_NORMED)
 
 #finding the occurences of the template
 threshold=0.0135
-templateHeight,templateWidth= templateGrey.shape
+templateHeight,templateWidth=templateGrey.shape
 
 #checking the normalized squared difference value is smaller than threshold value so thats its closer to the template or equal to the template
 location=np.where(result <= threshold)
 
-#drawing the rectangle
+#creating black mask for the size of the image
+mask=np.zeros(imageGrey.shape,dtype=np.uint8)
+
+
+#drawing the rectangle on the mask
 for point in zip(*location[::-1]):
-    cv2.rectangle(imageGrey, point,(point[0]+templateWidth,point[1]+templateHeight),0,2)
+    cv2.rectangle(mask, point,(point[0]+templateWidth,point[1]+templateHeight),255,-1)
+
+#applying the mask ontop of the image
+finalimage=cv2.bitwise_and(imageGrey,imageGrey,mask=mask)
 
 #Displaying the image
-cv2.imshow("Detected Template",imageGrey)
+
 cv2.imshow("Template",templateGrey)
+cv2.imshow("Template Detected",finalimage)
 cv2.waitKey(0)
